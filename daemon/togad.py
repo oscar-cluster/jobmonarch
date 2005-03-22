378225c2363666a6a0c8724ad45f53db3f2252d5
bastiaans <ramon.bastiaans@gmail.com>
2005-03-22 11:48:19 +0000

    daemon/togad.py:
    	First attempt at rrd handling
    	- Need to parse gmetad.conf first: class added

--- daemon/togad.py
+++ daemon/togad.py
@@ -4,13 +4,32 @@ from xml.sax import make_parser
 from xml.sax.handler import ContentHandler 
 import socket
 import sys
+import rrdtool
+import string
 
 # Specify debugging level here;
 #
 # >10 = metric XML
 # >9  = host,cluster,grid,ganglia XML
+# >8  = RRD activity,gmetad config parsing
 #
 DEBUG_LEVEL = 9
+
+# Where is the gmetad.conf located
+#
+GMETAD_CONF = '/etc/gmetad.conf'
+
+# Where to store the archived rrd's
+#
+ARCHIVE_PATH = '/data/toga/rrds'
+
+# List of data_source names to archive for
+#
+ARCHIVE_SOURCES = [ "LISA Cluster" ]
+
+#
+# Configuration ends
+#
 
 """
 This is TOrque-GAnglia's data Daemon
@@ -35,12 +54,15 @@ class GangliaXMLHandler( ContentHandler ):
 
 		elif name == 'CLUSTER':
 			self.clusterName = attrs.get('NAME',"")
+			self.rrd = RRDHandler( self.clusterName )
 			if (DEBUG_LEVEL>9): print ' |-Cluster found: %s' %( self.clusterName )
 
 		elif name == 'HOST':     
 			self.hostName = attrs.get('NAME',"")
 			self.hostIp = attrs.get('IP',"")
 			self.hostReported = attrs.get('REPORTED',"")
+			# Reset the metrics list for each host
+			self.metrics = [ ]
 			if (DEBUG_LEVEL>9): print ' | |-Host found: %s - ip %s reported %s' %( self.hostName, self.hostIp, self.hostReported )
 
 		elif name == 'METRIC':
@@ -53,16 +75,26 @@ class GangliaXMLHandler( ContentHandler ):
 
 		return
 
-	#def endElement( self, name ):
+	def endElement( self, name ):
 		#if name == 'ganglia_xml':
 
 		#if name == 'grid':
 
 		#if name == 'cluster':
 
-		#if name == 'host':     
+		if name == 'host':     
+			self.storeMetrics( self.hostName )
 
 		#if name == 'metric':
+
+	def storeMetrics( self, hostname ):
+
+		for metric in self.metrics:
+			self.rrd.checkCreate( hostname, metric['name'] )	
+			self.rrd.update( hostname, metric['name'], metric['val'] )
+			if (DEBUG_LEVEL>8): print 'stored metric %s for %s: %s' %( hostname, metric['name'], metric['val'] )
+			sys.exit(1)
+	
 
 class GangliaXMLGatherer:
 	"Setup a connection and file object to Ganglia's XML"
@@ -106,7 +138,7 @@ class GangliaXMLGatherer:
 
 class GangliaXMLProcessor:
 
-	def daemon(self):
+	def daemon( self ):
 		"Run as daemon forever"
 
 		self.DAEMON = 1
@@ -142,7 +174,7 @@ class GangliaXMLProcessor:
 
 		self.run()
 
-	def run(self):
+	def run( self ):
 		"Main thread"
 
 		while ( 1 ):
@@ -160,11 +192,98 @@ class GangliaXMLProcessor:
 
 		myParser.parse( myXMLGatherer.getFileObject() )
 
+class GangliaConfigParser:
+
+	sources = [ ]
+
+	def __init__( self, config ):
+		self.config = config
+		self.parseValues()
+
+	def parseValues(self):
+		"Parse certain values from gmetad.conf"
+
+		readcfg = open( self.config, 'r' )
+
+		for line in readcfg.readlines():
+
+			if line.count( '"' ) > 1:
+
+				source = { }
+				source['name'] = line.split( '"' )[1]
+
+				if line.find( 'data_source' ) and line[0] != '#':
+
+					source_words = line.split( '"' )[2].split( ' ' )
+					print line.split( '"' )
+					print source_words 
+
+					for word in source_words:
+
+						print 'word %s' %word
+						valid_interval = 1
+
+						for letter in word:
+							print 'letter %s' %letter
+							if letter not in string.digits:
+								valid_interval = 0
+
+						if valid_interval:
+							source['interval'] = word
+							if (DEBUG_LEVEL>8): print 'polling interval for %s = %s' %(source['name'], source['interval'] )
+		
+		# No interval found, use Ganglia's default	
+		if not source.has_key( 'interval' ):
+			source['interval'] = 15
+
+		self.sources.append( source )
+
+	def getInterval( self, source_name ):
+		for source in self.sources:
+			if source['name'] == name:
+				return source['interval']
+		return None
+
+class RRDHandler:
+
+	def __init__( self, cluster ):
+		self.cluster = cluster
+
+		self.gmetad_conf = GangliaConfigParser( GMETAD_CONF )
+
+	def createCheck( self, host, metric ):
+		"Check if an .rrd allready exists for this metric, create if not"
+
+		rrd_dir = '%s/%s/%s' %( check_dir(ARCHIVE_PATH), self.cluster, host )
+
+		if not os.path.exists( rrd_dir ):
+			os.makedirs( rrd_dir )
+
+		rrd_file = '%s/%s.rrd' %( rrd_dir, metric )
+
+		interval = self.gmetad_conf.getInterval( self.cluster )
+
+	def update( self, metric, timestamp, val ):
+
+		pass
+
+		#rrd.update( bla )
+		
+
 def main():
 	"Program startup"
 
 	myProcessor = GangliaXMLProcessor()
 	myProcessor.processXML()
 
+def check_dir( directory ):
+	"Check if directory is a proper directory. I.e.: Does _not_ end with a '/'"
+
+	if directory[-1] == '/':
+		directory = directory[:-1]
+
+	return directory
+
 # Let's go
-main()
+if __name__ == '__main__':
+	main()
