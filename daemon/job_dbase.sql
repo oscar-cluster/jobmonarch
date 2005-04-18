8affbae96f8cbeb51a9691088ef67b60390923d2
bastiaans <ramon.bastiaans@gmail.com>
2005-04-18 09:55:36 +0000

    daemon/make_dbase.sh:
    	- Shell script for creating dbase
    
    daemon/job_dbase.sql:
    
    	- Typo in syntax

--- daemon/job_dbase.sql
+++ daemon/job_dbase.sql
@@ -2,7 +2,7 @@ DROP TABLE jobs CASCADE;
 DROP TABLE nodes CASCADE;
 DROP TABLE job_nodes CASCADE;
 
-CREATE TABLE jobs {
+CREATE TABLE jobs (
 	job_id			INT NOT NULL UNIQUE PRIMARY KEY,
 	job_name		VARCHAR(100),
 	job_queue		VARCHAR(50),
@@ -13,18 +13,21 @@ CREATE TABLE jobs {
 	job_status		VARCHAR(2),
 	job_start_timestamp	VARCHAR(10),
 	job_stop_timestamp	VARCHAR(10)
-};
+);
 
-CREATE TABLE nodes {
+CREATE TABLE nodes (
 	node_id			SERIAL NOT NULL UNIQUE PRIMARY KEY,
 	node_hostname		VARCHAR(100)
-};
+);
 
-CREATE TABLE job_nodes {
+CREATE TABLE job_nodes (
 	job_id			INT NOT NULL REFERENCES jobs ON DELETE RESTRICT,
 	node_id			INT NOT NULL REFERENCES nodes ON DELETE RESTRICT,
 	PRIMARY KEY ( job_id, node_id )
-};
+);
+
+CREATE USER root;
+CREATE USER ramon;
 
 GRANT ALL ON jobs,nodes,job_nodes TO root,ramon;
 GRANT ALL ON nodes_node_id_seq TO root,ramon;
