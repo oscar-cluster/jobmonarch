9a38573b399de609a287159a82febd968304d505
bastiaans <bastiaans@47a44237-64ba-0310-a4ba-a5e43e82456e>
2006-04-07 10:42:02 +0000

    web/addons/job_monarch/conf.php:
    	- added option COLUMN_REQUESTED_MEMORY to enable/disable showing req. mem
    
    web/addons/job_monarch/version.php:
    
    	- version increase to 0.1.1
    
    web/addons/job_monarch/templates/overview.tpl:
    
    	- req. mem column is now a template block
    	- added template block for queued since column (not yet used)
    
    web/addons/job_monarch/overview.php:
    
    	- req. mem column will now only be displayed if enabled in conf.php
    
    web/addons/job_monarch/footer.php:
    
    	- removed some bogus

--- trunk/web/addons/job_monarch/footer.php
+++ trunk/web/addons/job_monarch/footer.php
@@ -25,9 +25,9 @@
 $tpl = new TemplatePower( "templates/footer.tpl" );
 $tpl->prepare();
 $tpl->assign("webfrontend-version",$version["webfrontend"]);
-$tpl->assign("togaweb-version", "0.1");
-$tpl->assign("togaarch-version", "0.1");
-$tpl->assign("togaplug-version", "0.1");
+//$tpl->assign("togaweb-version", "0.1.1");
+//$tpl->assign("togaarch-version", "0.1.1");
+//$tpl->assign("togaplug-version", "0.1.1");
 
 if ($version["gmetad"]) {
    $tpl->assign("webbackend-component", "gmetad");
