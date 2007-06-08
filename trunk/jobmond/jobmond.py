7d7437bf61cc8b89a80f0ca80f28e4a53a1e6cec
bastiaans <bastiaans@47a44237-64ba-0310-a4ba-a5e43e82456e>
2007-06-08 14:25:07 +0000

    jobmond/jobmond.py:
     * integrated native gmetric
       thanks to: Nick Galbreath
    
    jobmond/gmetric.py:
    
     * moved into jobmond.py
    
    jobmond/LICENSE.gmetric:
    
     * added gmetric LICENSE

--- trunk/jobmond/jobmond.py
+++ trunk/jobmond/jobmond.py
@@ -23,6 +23,7 @@
 
 import sys, getopt, ConfigParser
 import time, os, socket, string, re
+import xdrlib, socket
 import xml, xml.sax
 from xml.sax import saxutils, make_parser
 from xml.sax import make_parser
@@ -824,6 +825,85 @@ class PbsDataGatherer( DataGatherer ):
 			str_list.append( my_val_str )
 
 		return str_list
+
+GMETRIC_DEFAULT_TYPE    = 'string'
+GMETRIC_DEFAULT_HOST    = '127.0.0.1'
+GMETRIC_DEFAULT_PORT    = '8649'
+
+class Gmetric:
+
+	global GMETRIC_DEFAULT_HOST, GMETRIC_DEFAULT_PORT
+
+	slope           = { 'zero' : 0, 'positive' : 1, 'negative' : 2, 'both' : 3, 'unspecified' : 4 }
+	type            = ( '', 'string', 'uint16', 'int16', 'uint32', 'int32', 'float', 'double', 'timestamp' )
+	protocol        = ( 'udp', 'multicast' )
+
+	def __init__( self, host=GMETRIC_DEFAULT_HOST, port=GMETRIC_DEFAULT_PORT ):
+                
+		global GMETRIC_DEFAULT_TYPE
+
+		self.prot       = self.checkHostProtocol( host )
+		self.msg        = xdrlib.Packer()
+		self.socket     = socket.socket( socket.AF_INET, socket.SOCK_DGRAM )
+
+		if self.prot not in self.protocol:
+
+			raise ValueError( "Protocol must be one of: " + str( self.protocol ) )
+
+		if self.prot == 'multicast':
+
+			self.socket.setsockopt( socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 20 )
+
+		self.hostport   = ( host, int( port ) )
+		self.type       = GMETRIC_DEFAULT_TYPE
+		self.unitstr    = ''
+		self.slopestr   = 'both'
+		self.tmax       = 60
+
+	def checkHostProtocol( self, ip ):
+
+		MULTICAST_ADDRESS_MIN   = ( "224", "0", "0", "0" )
+		MULTICAST_ADDRESS_MAX   = ( "239", "255", "255", "255" )
+
+		ip_fields               = ip.split( '.' )
+
+		if ip_fields >= MULTICAST_ADDRESS_MIN and ip_fields <= MULTICAST_ADDRESS_MAX:
+
+			return 'multicast'
+		else:
+			return 'udp'
+
+	def send( self, name, value, dmax ):
+
+		msg             = self.makexdr( name, value, self.type, self.unitstr, self.slopestr, self.tmax, dmax )
+
+		return self.socket.sendto( msg, self.hostport )
+
+	def makexdr( self, name, value, typestr, unitstr, slopestr, tmax, dmax ):
+
+		if slopestr not in self.slope:
+
+			raise ValueError( "Slope must be one of: " + str( self.slope.keys() ) )
+
+		if typestr not in self.type:
+
+			raise ValueError( "Type must be one of: " + str( self.type ) )
+
+		if len( name ) == 0:
+
+			raise ValueError( "Name must be non-empty" )
+
+		self.msg.reset()
+		self.msg.pack_int( 0 )
+		self.msg.pack_string( typestr )
+		self.msg.pack_string( name )
+		self.msg.pack_string( str( value ) )
+		self.msg.pack_string( unitstr )
+		self.msg.pack_int( self.slope[ slopestr ] )
+		self.msg.pack_uint( int( tmax ) )
+		self.msg.pack_uint( int( dmax ) )
+
+		return self.msg.get_buffer()
 
 def printTime( ):
 
