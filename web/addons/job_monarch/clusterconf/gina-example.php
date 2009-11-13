55df9dcb0302469dd26dff1aa493551dd6d6b091
ramonb <ramon.bastiaans@gmail.com>
2009-11-13 14:13:29 +0000

    job_monarch/libtoga.php, job_monarch/clusterconf/gina-example.php:
     * changed hostname x/y coord parsing to support regexp's

--- web/addons/job_monarch/clusterconf/gina-example.php
+++ web/addons/job_monarch/clusterconf/gina-example.php
@@ -1,7 +1,7 @@
 <?php
 //
 //$SORTBY_HOSTNAME = "v{x}-{y}.gina.sara.nl";
-$SORTBY_HOSTNAME = "{x}-{y}.gina.sara.nl";
+$SORTBY_HOSTNAME = "[a-z,A-Z]+{x}-{y}.gina.sara.nl";
 $SORT_ORDER = "asc";
 
 $SORT_XLABEL = "rack";
