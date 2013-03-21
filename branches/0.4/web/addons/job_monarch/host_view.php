7e9ad33b8c0580c473a945f0f4703b1e5967b194
ramonb <ramonb@47a44237-64ba-0310-a4ba-a5e43e82456e>
2013-03-21 20:53:01 +0000

    * cleanup

--- branches/0.4/web/addons/job_monarch/host_view.php
+++ branches/0.4/web/addons/job_monarch/host_view.php
@@ -24,13 +24,9 @@
 
 include_once "./libtoga.php";
 
-//$tpl = new TemplatePower( "templates/host_view.tpl" );
-//$tpl->assignInclude("extra", "templates/host_extra.tpl");
-//$tpl->prepare();
+function datetimeToEpoch( $datetime ) 
+{
 
-function datetimeToEpoch( $datetime ) {
-
-        //printf("datetime = %s\n", $datetime );
         $datetime_fields = explode( ' ', $datetime );
 
         $date = $datetime_fields[0];
@@ -50,25 +46,18 @@ function datetimeToEpoch( $datetime ) {
         $minutes = $time_fields[1];
         $seconds = $time_fields[2];
 
-        //printf( "hours = %s minutes = %s seconds = %s\n", $hours, $minutes, $seconds );
-
         $timestamp = mktime( $hours, $minutes, $seconds, $months, $days, $years );
-
-        //printf( "timestamp = %s\n", $timestamp );
 
         return $timestamp;
 }
 
-function makeHostView() {
+function makeHostView() 
+{
 
     global $tpl, $metrics, $clustername, $hostname;
     global $cluster_ul, $hosts_up, $get_metric_string;
     global $cluster, $period_start, $period_stop;
     global $job_start, $job_stop;
-
-    //print_r( $metrics );
-
-    //printf( "c %s\n", $clustername );
 
     $trd = new TarchRrdGraph( $clustername, $hostname );
     $rrdirs = $trd->getRrdDirs( $period_start, $period_stop );
@@ -99,7 +88,6 @@ function makeHostView() {
     }
 
     $hosts_up = $hosts_up[$hostname];
-    //print_r( $hosts_up );
 
     $tpl->assign("cluster", $clustername);
     $tpl->assign("host", $hostname);
@@ -107,10 +95,12 @@ function makeHostView() {
     $tpl->assign("sort",$sort);
     $tpl->assign("range",$range);
 
-    if( !is_numeric( $period_start ) ) {
+    if( !is_numeric( $period_start ) ) 
+    {
         $period_start = datetimeToEpoch( $period_start );
     }
-    if( !is_numeric( $period_stop ) ) {
+    if( !is_numeric( $period_stop ) ) 
+    {
         $period_stop = datetimeToEpoch( $period_stop );
     }
 
@@ -121,48 +111,41 @@ function makeHostView() {
 
     $cluster_url=rawurlencode($clustername);
     $tpl->assign("cluster_url", $cluster_url);
-    //$tpl->assign("graphargs", "h=$hostname&$get_metric_string&st=$cluster[LOCALTIME]&job_start=$job_start&job_stop=$job_stop&period_start=$period_start&period_stop=$period_stop");
     $tpl->assign("graphargs", "h=$hostname&$get_metric_string&job_start=$job_start&job_stop=$job_stop&period_start=$period_start&period_stop=$period_stop");
 
     # For the node view link.
     $tpl->assign("node_view","./?p=2&c=$cluster_url&h=$hostname");
 
-    //# No reason to go on if this node is down.
-    //if ($hosts_down)
-    //   {
-    //      $tpl->printToScreen();
-    //      return;
-    //   }
-
     $tpl->assign("ip", $hosts_up[IP]);
 
     foreach ($mymetrics as $name => $v)
-       {
-           if ($v[TYPE] == "string" or $v[TYPE]=="timestamp" or $always_timestamp[$name])
-          {
-             # Long gmetric name/values will disrupt the display here.
-             if ($v[SOURCE] == "gmond") $s_metrics[$name] = $v;
-          }
-           elseif ($v[SLOPE] == "zero" or $always_constant[$name])
-          {
-             $c_metrics[$name] = $v;
-          }
-           else if ($reports[$metric])
-          continue;
-           else
-          {
-             //$graphargs = "c=$cluster_url&h=$hostname&v=$v[VAL]&m=$name"
-             //  ."&z=medium&st=$cluster[LOCALTIME]&job_start=$job_start&job_stop=$job_stop&period_start=$period_start&period_stop=$period_stop";
-             $graphargs = "c=$cluster_url&h=$hostname&m=$name"
+    {
+        if ($v[TYPE] == "string" or $v[TYPE]=="timestamp" or $always_timestamp[$name])
+        {
+            # Long gmetric name/values will disrupt the display here.
+            if ($v[SOURCE] == "gmond") $s_metrics[$name] = $v;
+        }
+        elseif ($v[SLOPE] == "zero" or $always_constant[$name])
+        {
+            $c_metrics[$name] = $v;
+        }
+        else if ($reports[$metric])
+        {
+            continue;
+        }
+        else
+        {
+            $graphargs = "c=$cluster_url&h=$hostname&m=$name"
                ."&z=medium&job_start=$job_start&job_stop=$job_stop&period_start=$period_start&period_stop=$period_stop";
-             # Adding units to graph 2003 by Jason Smith <smithj4@bnl.gov>.
-             if ($v[UNITS]) {
-            $encodeUnits = rawurlencode($v[UNITS]);
-            $graphargs .= "&vl=$encodeUnits";
-             }
-             $g_metrics[$name][graph] = $graphargs;
-          }
-       }
+            # Adding units to graph 2003 by Jason Smith <smithj4@bnl.gov>.
+            if ($v[UNITS]) 
+            {
+                $encodeUnits = rawurlencode($v[UNITS]);
+                $graphargs .= "&vl=$encodeUnits";
+            }
+            $g_metrics[$name][graph] = $graphargs;
+        }
+    }
     # Add the uptime metric for this host. Cannot be done in ganglia.php,
     # since it requires a fully-parsed XML tree. The classic contructor problem.
     $s_metrics[uptime][TYPE] = "string";
@@ -177,51 +160,52 @@ function makeHostView() {
 
     # Show string metrics
     if (is_array($s_metrics))
-       {
-          ksort($s_metrics);
-          foreach ($s_metrics as $name => $v )
-         {
-        $tpl->newBlock("string_metric_info");
-        $tpl->assign("name", $name);
-        if( $v[TYPE]=="timestamp" or $always_timestamp[$name])
-           {
-              $tpl->assign("value", date("r", $v[VAL]));
-           }
-        else
-           {
-              $tpl->assign("value", "$v[VAL] $v[UNITS]");
-           }
-         }
-       }
+    {
+        ksort($s_metrics);
+        foreach ($s_metrics as $name => $v )
+        {
+            $tpl->newBlock("string_metric_info");
+            $tpl->assign("name", $name);
+            if( $v[TYPE]=="timestamp" or $always_timestamp[$name])
+            {
+                $tpl->assign("value", date("r", $v[VAL]));
+            }
+            else
+            {
+                $tpl->assign("value", "$v[VAL] $v[UNITS]");
+            }
+        }
+    }
 
     # Show constant metrics.
     if (is_array($c_metrics))
-       {
-          ksort($c_metrics);
-          foreach ($c_metrics as $name => $v )
-         {
-        $tpl->newBlock("const_metric_info");
-        $tpl->assign("name", $name);
-        $tpl->assign("value", "$v[VAL] $v[UNITS]");
-         }
-       }
+    {
+        ksort($c_metrics);
+        foreach ($c_metrics as $name => $v )
+        {
+            $tpl->newBlock("const_metric_info");
+            $tpl->assign("name", $name);
+            $tpl->assign("value", "$v[VAL] $v[UNITS]");
+        }
+    }
 
     # Show graphs.
     if (is_array($g_metrics))
-       {
-          ksort($g_metrics);
+    {
+        ksort($g_metrics);
 
-          $i = 0;
-          foreach ( $g_metrics as $name => $v )
-         {
+        $i = 0;
+        foreach ( $g_metrics as $name => $v )
+        {
             $tpl->newBlock("vol_metric_info");
             $tpl->assign("graphargs", $v[graph]);
             $tpl->assign("alt", "$hostname $name");
             if($i++ %2)
-               $tpl->assign("br", "<BR>");
+            {
+                $tpl->assign("br", "<BR>");
+            }
          }
-       }
+    }
 }
 
-    //$tpl->printToScreen();
 ?>
