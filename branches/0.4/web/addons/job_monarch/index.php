b3425f67a7a3aa092f60ab3c4c5faf8da758ca76
ramonb <ramonb@47a44237-64ba-0310-a4ba-a5e43e82456e>
2013-03-26 20:40:00 +0000

    job_monarch/index.php:
     * bring back anti cache headers
    
    job_monarch/templates/index.tpl:
    
     * no longer needed with Dwoo

--- branches/0.4/web/addons/job_monarch/index.php
+++ branches/0.4/web/addons/job_monarch/index.php
@@ -423,10 +423,10 @@ function makeHeader( $page_call, $title, $longtitle )
     $tpl_data->assign("node_menu", $node_menu);
 
     # Make sure that no data is cached..
-    //header ("Expires: Mon, 26 Jul 1997 05:00:00 GMT");    # Date in the past
-    //header ("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT"); # always modified
-    //header ("Cache-Control: no-cache, must-revalidate");  # HTTP/1.1
-    //header ("Pragma: no-cache");                          # HTTP/1.0
+    header ("Expires: Mon, 26 Jul 1997 05:00:00 GMT");    # Date in the past
+    header ("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT"); # always modified
+    header ("Cache-Control: no-cache, must-revalidate");  # HTTP/1.1
+    header ("Pragma: no-cache");                          # HTTP/1.0
 
     $dwoo->output($tpl, $tpl_data);
 }
--- branches/0.4/web/addons/job_monarch/templates/index.tpl
+++
@@ -1,3 +0,0 @@
-{include(file="$header")}
-{include(file="$main")}
-{include(file="$footer")}
